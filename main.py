397e62da9a5051d3a1c755118677264553f2b1ec
Anonymized <anonymized@anonymized.com>
2019-03-12 22:58:06 -0400

    fix url_ time_ and loc_index_batch

--- main.py
+++ main.py
@@ -24,14 +24,24 @@ if __name__ == '__main__':
 	time_index_batch = []
 	loc_index_batch = []
 	for user in user_batch:
-		url_index_batch.append(url2id[user['url']])
-		time_index_batch.append(time2id[user['time']])
-		loc_index_batch.append(loc2id[user['loc']])
+		user_urls = user['urls'] # urls of behaviors
+		user_times = user['times']
+		user_locs = user['locs']
+		assert len(user_urls) == len(user_times) == len(user_locs)
+		for i, url in enumerate(user_urls):
+			user_urls.append(url2id[url])
+			user_times.append(time2id[user_times[i]])
+			user_locs.append(url2id[user_locs[i]])
+		url_index_batch.append(user_urls)
+		time_index_batch.append(user_times)
+		loc_index_batch.append(user_locs)
 
 	behavior_cnn = Behaviors_CNN(in_channels=1, total_num_URL=len(url2id), total_num_TIME=len(time2id), total_num_LOC=len(loc2id), out_channels=50, filter_widths=[3,4,5])
 	behavior_cnn.to(device)
 	fnn = FFNN(input_size=50*3, hidden_size=50, num_hidden_layers=1, output_size=2)
 
+	# [batch_size, max_sum_behaviors, embedding_size]
+	# TO DO: padding for url_index_batch, because different user may has diffrent number of behaviors
 	url_index_batch = torch.LongTensor(url_index_batch).to(device)
 	time_index_batch = torch.LongTensor(time_index_batch).to(device)
 	loc_index_batch = torch.LongTensor(loc_index_batch).to(device)
